aa446f04d30e88c8dba999a338c08ac23a0e5df9
Poojkulk <50866928+Poojkulk@users.noreply.github.com>
2020-11-24 12:44:30 +0530

    Update index.js

--- views/themes/Cloth/index.js
+++ views/themes/Cloth/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-arrow-callback, no-var, no-tabs */
 $(document).ready(function (){
     // Add specific code to this theme here
+    p "welcome to node js"
 });
 
 p "welcome to node js"
